b2caffbab5f3416ddbab3a538f251c6e27424d13
Matthew Pagan <mastapegs01@gmail.com>
2023-12-06 12:59:46 -0500

    add logs for instructions

--- src/challenge_1/challenge_1.ts
+++ src/challenge_1/challenge_1.ts
@@ -9,6 +9,8 @@ function isCharacterANumber(char: string) {
 
 const calibrationValueFromLine = (line: string): number => {
   let calibrationValueString = "";
+
+  // 4. For each line find the first and last number
   for (let i = 0; i < line.length; i++) {
     const char = line[i];
     const charIsANumber = isCharacterANumber(char);
@@ -23,24 +25,30 @@ const calibrationValueFromLine = (line: string): number => {
     calibrationValueString = `${calibrationValueString}${char}`;
     break;
   }
+
+  // 5. Return the calibration value
   return Number(calibrationValueString);
 };
 
 export const challenge_1 = async () => {
+  // 1. Grab file containing lines with calibration values
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const pathName = path.join(__dirname, "../../data/challenge_1_data.txt");
 
+  // 2. Prepare to read from file, line by line
   const fileStream = fs.createReadStream(pathName);
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
 
+  // 3. Read each line, and extract the calibration value, and add it to sum
   let calibrationValueSum = 0;
   for await (const line of rl) {
     calibrationValueSum += calibrationValueFromLine(line);
   }
 
+  // 6. Log the sum of all calibration values
   console.log({ calibrationValueSum });
 };
