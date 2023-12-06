f9c3d93305612a8e4f3a693c5bc020b2b9afc3e3
Matthew Pagan <mastapegs01@gmail.com>
2023-12-06 11:29:01 -0500

    can now read from file

--- src/challenge_1/challenge_1.ts
+++ src/challenge_1/challenge_1.ts
@@ -1,12 +1,20 @@
 import fs from "fs";
+import path from "path";
 import readline from "readline";
+import { fileURLToPath } from "url";
 
 export const challenge_1 = async () => {
-  const fileStream = fs.createReadStream("./sample_data.txt");
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathName = path.join(__dirname, "../../data/sample_data.txt");
+  console.log({ pathName });
+
+  const fileStream = fs.createReadStream(pathName);
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
+
   for await (const line of rl) {
     console.log(`Line from file: ${line}`);
   }
